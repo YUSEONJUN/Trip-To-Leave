256788f0fb927750e4aa95c115aa4e5f63844c60
ParkJaeHyeon <wogus11254@hs.ac.kr>
2023-05-08 13:21:53 +0900

    Desktop Test

---
+++ src/main/java/com/example/demo/Test4.java
@@ -0,0 +1,10 @@
+package com.example.demo;
+
+import org.springframework.boot.SpringApplication;
+
+public class Test4 {
+    public static void main(String[] args) {
+        SpringApplication.run(Test4.class, args);
+
+    }
+}
