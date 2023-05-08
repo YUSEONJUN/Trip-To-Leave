3f7546e82ab4f64d022cccd10000f5b414b05cb3
ParkJaeHyeon <wogus11254@hs.ac.kr>
2023-05-08 13:25:03 +0900

    Desktop Test2

--- src/main/java/com/example/demo/Test5.java
+++ src/main/java/com/example/demo/Test5.java
@@ -2,9 +2,9 @@ package com.example.demo;
 
 import org.springframework.boot.SpringApplication;
 
-public class Test4 {
+public class Test5 {
     public static void main(String[] args) {
-        SpringApplication.run(Test4.class, args);
+        SpringApplication.run(Test5.class, args);
 
     }
 }
