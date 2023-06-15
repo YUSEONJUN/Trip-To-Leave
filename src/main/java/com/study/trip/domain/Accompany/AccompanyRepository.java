b2c6a04500ea4db37894520d0a7ce7e7e268b363
yuseonjun <seonjun0906@gmail.com>
2023-06-15 15:59:48 +0900

    refactor

--- src/main/java/com/study/trip/domain/Accompany/AccompanyRepository.java
+++
@@ -1,8 +0,0 @@
-package com.study.trip.domain.Accompany;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface AccompanyRepository extends JpaRepository<Accompany, Long> {
-
-
-}
